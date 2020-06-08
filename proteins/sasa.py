d853e77ffb0e1052a8525e914b53717e3e40a933
Dave Juergens <davidcj@uw.edu>
2020-06-08 10:25:04 -0700

    Started SASA script for burial analysis

---
+++ proteins/sasa.py
@@ -0,0 +1,42 @@
+"""
+Module for finding solvent accessible surface area for residues/atoms in proteins,
+and other burial related analysis
+"""
+import sys
+import os
+
+from pyrosetta import *
+from pyrosetta.rosetta import *
+
+
+def residue_sasas(pdb):
+    """
+    Calculates the solvent accessible surface areas for all residues in pdb. Returns
+    dictionary of corresponding sasas
+
+    Parameters:
+        pdb (str, required): path to the pdb to be analyzed
+
+    Returns:
+        dict: keys are residue number, values are solven accesible surface area 
+    """
+
+    # create pose, rename sasa calculator
+    pose = pose_from_pdb(pdb)
+    sasa_calc = protocols.vardist_solaccess.VarSolDistSasaCalculator()
+    # create sasas map
+    sasa_map = sasa_calc.calculate(pose)
+
+    # dictionary to store the resulting sasas
+    residue_sasas = {}
+
+    for i in range(1, sasa_map.size()+1):
+        atom_sasas = sasa_map[i]
+
+        for j in range(1, len(atom_sasas)+1):
+            if j == 1:
+                residue_sasas[i] = atom_sasas[j]
+            else:
+                residue_sasas[i] += atom_sasas[j]
+
+    return residue_sasas
